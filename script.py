442f6aa3a9a2232e62026fffca465e2a126092b3
Siavash Khallaghi <siavash.khallaghi@prenuvo.com>
2024-03-01 15:56:30 -0800

    Changed the add_two function to add_value to support ticket SKYN-123.

--- script.py
+++ script.py
@@ -1,12 +1,24 @@
-def add_two(num: int) -> int:
-    return num + 2
+def add_value(num: int, value: int) -> int:
+    if not isinstance(value, int):
+        raise ValueError(f"Error: only integer values are supported got {value}")
+
+    if not isinstance(num, int):
+        raise ValueError(f"Error: only integer values are supported got {num}")
+
+    if value == 2:
+        return num + 4
+    elif value == 5:
+        return num + 2 * value
+    else:
+        return num + value
 
 
 def main():
     x = 10
-    y = add_two(x)
+    v = 5
+    y = add_value(x, v)
 
-    print(x, y)
+    print(x, v, y)
 
 
 if __name__ == '__main__':
