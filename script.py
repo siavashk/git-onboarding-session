2ff2fd7799033064c68f534f984162e4fd961073
Siavash Khallaghi <siavash.khallaghi@prenuvo.com>
2024-03-01 16:06:29 -0800

    Updated add_two function to support strings and sets as well.

--- script.py
+++ script.py
@@ -1,10 +1,29 @@
-def add_two(num: int) -> int:
+from copy import deepcopy
+from typing import Union
+
+
+def add_two(num: Union[int, str, set]) -> int:
+    if isinstance(num, str):
+        return f"{num} + 2"
+    elif isinstance(num, set):
+        num.add(2)
+        return num
+
     return num + 2
 
 
 def main():
     x = 10
     y = add_two(x)
+
+    print(x, y)
+
+    x = "foo"
+    y = add_two(x)
+    print(x, y)
+
+    x = set()
+    y = add_two(deepcopy(x))
 
     print(x, y)
 
